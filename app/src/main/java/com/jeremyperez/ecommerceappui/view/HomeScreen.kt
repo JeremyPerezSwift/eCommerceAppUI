bcb3cea46d5ea946ec9d6aa2a8788dde08763bd4
JeremyPerezSwift <jeremyperez34000@gmail.com>
2026-03-24 11:09:59 +0100

    feat(ui): add BottomBar composable and initialize HomeScreen

---
+++ app/src/main/java/com/jeremyperez/ecommerceappui/view/HomeScreen.kt
@@ -0,0 +1,58 @@
+package com.jeremyperez.ecommerceappui.view
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.jeremyperez.ecommerceappui.R
+
+@Preview
+@Composable
+fun HomeScreen() {
+
+}
+
+@Preview
+@Composable
+fun BottomBar() {
+    val bottomBarIcons = listOf(
+        R.drawable.home_icon,
+        R.drawable.categories_icon,
+        R.drawable.wishlist_icon,
+        R.drawable.profile_icon,
+    )
+
+    Box(
+        Modifier.fillMaxWidth().wrapContentHeight(),
+        contentAlignment = Alignment.BottomCenter,
+    ) {
+        Row(
+            Modifier.fillMaxWidth()
+                .background(Color.White)
+                .padding(20.dp),
+            horizontalArrangement = Arrangement.spacedBy(60.dp)
+        ) {
+            bottomBarIcons.forEachIndexed { index, icon ->
+                Image(painter = painterResource(icon), null, Modifier.size(24.dp))
+                if(index == 1)
+                    Spacer(Modifier.width(20.dp))
+            }
+        }
+
+        Image(painter = painterResource(R.drawable.center_tab), null)
+    }
+}
