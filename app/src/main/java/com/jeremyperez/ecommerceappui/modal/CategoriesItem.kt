c8c9a56ef1f1b47655643b9e08df21c642d143be
JeremyPerezSwift <jeremyperez34000@gmail.com>
2026-04-01 16:41:30 +0200

    feat(model): add CategoriesItem data model

---
+++ app/src/main/java/com/jeremyperez/ecommerceappui/modal/CategoriesItem.kt
@@ -0,0 +1,9 @@
+package com.jeremyperez.ecommerceappui.modal
+
+import androidx.compose.ui.graphics.Color
+
+data class CategoriesItem(
+    val title: String,
+    val img: Int,
+    val bgColor: Color
+)
